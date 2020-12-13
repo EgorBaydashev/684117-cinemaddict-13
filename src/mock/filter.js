ee797c21012d37d48e14ed6e66a177f9c8ef6945
Egor@Egora <baidashevegor@yandex.ru>
2020-12-13 22:16:34 +0500

    Правки

--- src/mock/filter.js
+++ src/mock/filter.js
@@ -1,11 +1,11 @@
-const FilmToFilterMap = {
+const filmToFilterMap = {
   watchlist: (films) => films.filter((film) => film.isInWatchlist).length,
   history: (films) => films.filter((film) => film.isWatched).length,
   favorites: (films) => films.filter((film) => film.isFavorite).length
 };
 
 export const generateFilter = (films) => {
-  return Object.entries(FilmToFilterMap).map(([filterName, countFilms]) => {
+  return Object.entries(filmToFilterMap).map(([filterName, countFilms]) => {
     return {
       name: filterName,
       count: countFilms(films)
