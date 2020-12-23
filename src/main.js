5d2aaef9fb980e186dbb7715bef73694924b75f9
Egor@Egora <baidashevegor@yandex.ru>
2020-12-23 23:32:16 +0500

    Рефакторинг рендер-метода

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import {createPopupTemplate} from './view/popup.js';
 import {createStatsTemplate} from './view/stats.js';
 import {createFilmCard} from './mock/film-card.js';
 import {generateFilter} from './mock/filter.js';
+import {renderTemplate} from './utils.js';
 
 const CARDS_COUNT = 17;
 const FILMS_COUNT_PER_STEP = 5;
@@ -15,16 +16,12 @@ const FILMS_COUNT_PER_STEP = 5;
 let renderedFilmsCount = FILMS_COUNT_PER_STEP;
 const films = new Array(CARDS_COUNT).fill().map(createFilmCard);
 
-const render = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
-};
-
 const showMoreFilms = (evt) => {
   evt.preventDefault();
   films
     .slice(renderedFilmsCount, renderedFilmsCount + FILMS_COUNT_PER_STEP)
     .forEach((film) => {
-      render(filmsListContainer, createFilmCardTemplate(film), `beforeend`);
+      renderTemplate(filmsListContainer, createFilmCardTemplate(film), `beforeend`);
     });
 
   renderedFilmsCount += FILMS_COUNT_PER_STEP;
@@ -36,30 +33,30 @@ const showMoreFilms = (evt) => {
 
 const siteHeader = document.querySelector(`.header`);
 
-render(siteHeader, createUserProfileTemplate(), `beforeend`);
+renderTemplate(siteHeader, createUserProfileTemplate(), `beforeend`);
 
 const siteMain = document.querySelector(`.main`);
 
-render(siteMain, createSiteNavigationTemplate(generateFilter(films)), `beforeend`);
-render(siteMain, createSiteSortingTemplate(), `beforeend`);
-render(siteMain, createFilmListTemplate(), `beforeend`);
+renderTemplate(siteMain, createSiteNavigationTemplate(generateFilter(films)), `beforeend`);
+renderTemplate(siteMain, createSiteSortingTemplate(), `beforeend`);
+renderTemplate(siteMain, createFilmListTemplate(), `beforeend`);
 
 const filmsList = siteMain.querySelector(`.films-list`);
 const filmsListContainer = filmsList.querySelector(`.films-list__container`);
 
-render(filmsList, createShowMoreButtonTemplate(), `beforeend`);
+renderTemplate(filmsList, createShowMoreButtonTemplate(), `beforeend`);
 
 const showMoreButton = siteMain.querySelector(`.films-list__show-more`);
 
 showMoreButton.addEventListener(`click`, showMoreFilms);
 
 for (let i = 0; i < FILMS_COUNT_PER_STEP; i++) {
-  render(filmsListContainer, createFilmCardTemplate(films[i]), `beforeend`);
+  renderTemplate(filmsListContainer, createFilmCardTemplate(films[i]), `beforeend`);
 }
 
-render(siteMain, createPopupTemplate(films[0]), `beforeend`);
+renderTemplate(siteMain, createPopupTemplate(films[0]), `beforeend`);
 
-render(siteMain, createStatsTemplate(), `beforeend`);
+renderTemplate(siteMain, createStatsTemplate(), `beforeend`);
 
 const createFooterStatistics = (filmsCount) => {
   return `<p>${filmsCount} movies inside</p>`;
@@ -68,4 +65,4 @@ const createFooterStatistics = (filmsCount) => {
 const footerElement = document.querySelector(`footer`);
 const footerStatisticsElement = footerElement.querySelector(`.footer__statistics`);
 
-render(footerStatisticsElement, createFooterStatistics(films.length), `beforeend`);
+renderTemplate(footerStatisticsElement, createFooterStatistics(films.length), `beforeend`);
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,34 @@
 import dayjs from 'dayjs';
 
+export const RenderPosition = {
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`
+};
+
+export const renderElement = (container, element, place) => {
+  switch (place) {
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+  }
+};
+
+export const createElement = (template) => {
+  const newElement = document.createElement(`div`);
+  newElement.innerHTML = template;
+
+  return newElement.firstChild;
+};
+
+export const renderTemplate = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+
+
 export const getRandomInteger = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
