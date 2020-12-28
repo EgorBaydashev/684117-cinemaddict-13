e95429b96e6b6aa81852e5f99201b883a8108b02
Egor@Egora <baidashevegor@yandex.ru>
2020-12-29 02:42:04 +0500

    Правка абстрактного класса

--- src/view/film-list-empry.js
+++ src/view/film-list-empry.js
@@ -1,4 +1,4 @@
-import {createElement} from '../utils.js';
+import AbstractView from './abstract.js';
 
 const createFilmListEmptyTemplate = () => {
   return `<section class="films">
@@ -8,24 +8,8 @@ const createFilmListEmptyTemplate = () => {
 </section>`;
 };
 
-export default class FilmListEmpty {
-  constructor() {
-    this._element = null;
-  }
-
+export default class FilmListEmpty extends AbstractView {
   getTemplate() {
     return createFilmListEmptyTemplate();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
