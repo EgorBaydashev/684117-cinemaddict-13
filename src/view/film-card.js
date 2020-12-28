597d77ee1cbf99cd938ba8b60b916efa1bf5a38a
Egor@Egora <baidashevegor@yandex.ru>
2020-12-29 02:53:09 +0500

    Унаследованы сложные компоненты

--- src/view/film-card.js
+++ src/view/film-card.js
@@ -1,4 +1,4 @@
-import {createElement} from '../utils.js';
+import AbstractView from './abstract.js';
 
 const createFilmCardTemplate = (film) => {
   const {poster, title, rating, releaseYear, runtime, genres, description, comments, isInWatchlist, isWatched, isFavorite} = film;
@@ -47,25 +47,13 @@ const createFilmCardTemplate = (film) => {
   </article>`;
 };
 
-export default class FilmCard {
+export default class FilmCard extends AbstractView {
   constructor(film) {
+    super();
     this._film = film;
-    this._element = null;
   }
 
   getTemplate() {
     return createFilmCardTemplate(this._film);
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
--- src/view/popup.js
+++ src/view/popup.js
@@ -1,4 +1,4 @@
-import {createElement} from '../utils.js';
+import AbstractView from './abstract.js';
 
 const createPopupTemplate = (film) => {
   const {
@@ -175,25 +175,13 @@ const createPopupTemplate = (film) => {
 </section>`;
 };
 
-export default class Popup {
+export default class Popup extends AbstractView {
   constructor(film) {
+    super();
     this._film = film;
-    this._element = null;
   }
 
   getTemplate() {
     return createPopupTemplate(this._film);
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
